b9d9a18a37dea1fe5f1f44a6b9b230c4ea65fd9f
leyosu23 <leyosu23@gmail.com>
2021-06-20 19:00:34 -0400

    Initial Commit

---
+++ src/components/Todo.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Todo = ({text, todo, todos, setTodos}) => {
+    //Events
+    const deleteHandler = () => {
+        setTodos(todos.filter((el) => el.id !== todo.id));
+    };
+    const completeHandelr = () => {
+        setTodos(todos.map(item => {
+            if(item.id === todo.id) {
+                return {
+                    ...item, completed: !item.completed
+                };
+            }
+                return item;
+        }))
+    }
+
+    return(
+        <div className='todo'>
+            <li className={`todo-item ${todo.completed? "completed": ''}`}>{text}</li>
+            <button onClick={completeHandelr} className='complete-btn'>
+                <i className="fas fa-check"></i>
+            </button>
+            <button onClick={deleteHandler} className='trash-btn'>
+                <i className="fas fa-trash"></i>
+            </button>
+        </div>
+    );
+}
+
+export default Todo;
